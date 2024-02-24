2523a618a6645c03786ec118afd31091c1ba8134
AbdulMalek-swe <abdulmalek.swe.585@gmail.com>
2024-02-24 14:14:29 +0600

    update localstorage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,18 @@
 
 import ReCaptcha from '@/components/RecaptchaV2/Recaptcha';
 import RecaptchaV3 from '@/components/RecaptchaV2/RecaptchaV3';
+import Sortable from '@/components/Sortable/Sortable';
 import { useEffect } from 'react';
 
 export default function Home() {
 
 
   return (
-    <main className='text-left flex justify-start'>
+    <main className='text-left  '>
       <RecaptchaV3 />
       <ReCaptcha siteKey="6LeL7XwpAAAAAC8CrcaGBSZQMwvVzJwPvEOHdkNE" />
       {/* <div className='g-recaptcha bg-red-900' data-sitekey="6Lf_8n0pAAAAAM1ibs5TZ-Xw2_lwAyW70kQu2yO-" data-size="invisible" /> */}
+      <Sortable />
     </main>
   );
 }
---
+++ src/components/Sortable/Sortable.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const Sortable = () => {
+    const [data, setData] = useState<any>(JSON.parse(localStorage.getItem("person")) || [
+        {
+            id: 1, name: "Jhon don", ref: "1kobul"
+        },
+        {
+            id: 2, name: "Abbas", ref: "2none"
+        },
+        {
+            id: 3, name: "Sattar", ref: "1rafik",
+        }
+    ])
+    const dragPeople = useRef<number>(0)
+    const dragChangePeople = useRef<number>(0)
+    async function handleDrag() {
+        const person = [...data];
+        let temp = person[dragPeople.current];
+        person[dragPeople.current] = person[dragChangePeople.current];
+        person[dragChangePeople.current] = temp;
+        localStorage.setItem("person", JSON.stringify(person))
+        setData(person)
+
+    }
+    return (
+        <div className='flex justify-center flex-col  '>
+            {
+                data.map((item, index) => <div draggable
+
+                    onDragStart={() => dragPeople.current = index}
+                    onDragEnter={() => dragChangePeople.current = index}
+                    onDragEnd={handleDrag}
+                    onDragOver={e => e.preventDefault()}
+                    key={index} className='bg-gray-700 m-3 p-5 rounded-md hover:bg-red-400 cursor-pointer'>{item?.name}</div>)
+            }
+        </div>
+    );
+};
+
+export default Sortable;
